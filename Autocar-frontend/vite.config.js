42a70688ecbfacefc326eae028c45fdbd5ff5bdf
Vitor Hugo <vitor.cianorte00@gmail.com>
2024-03-15 13:54:28 -0300

    configs iniciais

--- Autocar-frontend/vite.config.js
+++ Autocar-frontend/vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(() => {
     return {
         plugins: [vue()],
+        base: "/autocar",
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
